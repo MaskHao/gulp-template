02694ca56ee57427abcab1be74e7e92198c1bade
mask_hao <mask_hao@163.com>
2018-02-06 17:36:51 +0800

    add files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    cssmin = require('gulp-clean-css'),
+//当发生异常时提示错误 确保本地安装gulp-notify和gulp-plumber
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    uglify = require('gulp-uglify');
 gulp.task('testLess', function () {
     gulp.src('src/less/*.less')
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
         .pipe(less())
+        .pipe(cssmin())  //兼容IE7及以下需设置compatibility属性 .pipe(cssmin({compatibility: 'ie7'}))
         .pipe(gulp.dest('src/css'));
 });
-gulp.task('default', ['testLess'])
+// gulp.task('default', ['testLess']);
+
+gulp.task('testWatch', function () {
+    gulp.watch('src/**/*.less', ['testLess']); //当所有less文件发生改变时，调用testLess任务
+});
+
+gulp.task('jsmin', function () {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
